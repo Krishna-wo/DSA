98eb2f734e24bf4e459d9c2274eadc93422d0166
Krishan Bhadala <krishanbhadala99@gmail.com>
2026-06-07 12:41:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (21.53%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -14,39 +14,25 @@
  * }
  */
 class Solution {
+    TreeNode xParent = null, yParent = null;
+    int xDepth = -1, yDepth = -1;
+
     public boolean isCousins(TreeNode root, int x, int y) {
-        Queue<TreeNode> q =new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int s= q.size();
-            boolean foundX = false;
-            boolean foundY = false;
-            for(int i=0;i<s;i++){
-                TreeNode n=q.poll();
-                if(n.val == x) foundX = true;
-                if(n.val == y) foundY = true;
-                
-                if(n.left!=null) q.add(n.left);
-                if(n.right!=null) q.add(n.right);
+        dfs(root, null, 0, x, y);
+        return (xDepth == yDepth) && (xParent != yParent);
+    }
 
-            }
-            if((foundX && foundY) && !isSibling(root, x, y)){
-                return true;
-            }
+    private void dfs(TreeNode node, TreeNode parent, int depth, int x, int y) {
+        if (node == null) return;
+        if (node.val == x) {
+            xParent = parent;
+            xDepth = depth;
         }
-        return false;
-        
+        if (node.val == y) {
+            yParent = parent;
+            yDepth = depth;
+        }
+        dfs(node.left, node, depth + 1, x, y);
+        dfs(node.right, node, depth + 1, x, y);
     }
-
-   boolean isSibling(TreeNode root, int x, int y) {
-    if(root == null) return false;
-    if(root.left != null && root.right != null) {
-        int l = root.left.val;
-        int r = root.right.val;
-        if((l== x && r==y)||(l==y && r == x))
-            return true;
-    }
-    return isSibling(root.left, x, y) ||
-           isSibling(root.right, x, y);
-}
 }
